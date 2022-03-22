0cf88b6dd2fc388dd337b9c17fd824afae59a32a
Vallej0 <georgev1980@gmail.com>
2022-03-22 18:38:35 -0500

    updated schema

--- schemas/nyt_table.sql
+++ schemas/nyt_table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE nyt_query (
-	id VARCHAR PRIMARY KEY,
-	ISBN_13 VARCHAR,
+    ISBN_13 VARCHAR primary key,
 	Title VARCHAR,
 	Author VARCHAR,
 	Publisher VARCHAR,
@@ -8,5 +7,5 @@ CREATE TABLE nyt_query (
 	NYT_Review_Link VARCHAR,
 	Most_Recent_Bestseller_Date VARCHAR,
 	Most_Recent_Rank VARCHAR,
-	Number_of_Weeks_on_List VARCHAR	
+	Number_of_Weeks_on_List VARCHAR
 )
